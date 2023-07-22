f77635a34a6a6ce810127df6e031fa67fc494e99
Alan Lazo <al4nlaz0@gmail.com>
2023-07-21 22:31:03 -0300

    added spanish language for errors and valitadion to register form

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -10,4 +10,13 @@ class RegisterController extends Controller
     public function index(){
         return view('auth.register');
     }
+
+    public function store(Request $request){
+        $this->validate($request, [
+            'name' => 'required |min:5',
+            'username'=> 'required|unique:users|min:3|max:30',
+            'email'=>'required|unique:users|email|max:60',
+            'passord'=>'required|confirmed|min:6',
+        ]);
+    }
 }
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -10,41 +10,77 @@
            <img src="{{asset('img/registrar.jpg')}}" alt="" srcset="">
         </div>
         <div class="md:w-4/12 bg-white p-6 rounded-lg shadow-xl">
-            <form>
+            <form action="{{route('register')}}" method="POST" nonvalidate>
+                @csrf
                 <div class="mb-5">
                     <label for="name" class="mb-4 block uppercase text-gray-500 font-bold">
                         Nombre
                     </label>
                     <input type="text" id="name" name="name" placeholder="Tu nombre"
-                    class=" border p-3 w-full rounded-lg">
+                    class=" border p-3 w-full rounded-lg @error('name')
+                        border-red-500
+                    @enderror">
+                    @error('name')
+                    <p class="bg-red-500 text-white my-2 rounded-lg text-sm p-2">
+                        {{$message}}
+                    </p>
+                    @enderror
                 </div>
                 <div class="mb-5">
                     <label for="username" class="mb-2 block uppercase text-gray-500 font-bold">
                         Username
                     </label>
                     <input type="text" id="username" name="username" placeholder="Tu nombre de usuario"
-                    class=" border p-3 w-full rounded-lg">
+                    class=" border p-3 w-full rounded-lg @error('username')
+                    border-red-500
+                @enderror">
+                    @error('username')
+                    <p class="bg-red-500 text-white my-2 rounded-lg text-sm p-2">
+                        {{$message}}
+                    </p>
+                    @enderror
                 </div>
                 <div class="mb-5">
                     <label for="email" class="mb-2 block uppercase text-gray-500 font-bold">
                         Email
                     </label>
                     <input type="email" id="email" name="email" placeholder="Tu Email de registro"
-                    class=" border p-3 w-full rounded-lg">
+                    class=" border p-3 w-full rounded-lg @error('email')
+                    border-red-500
+                @enderror"/>
+                    @error('email')
+                    <p class="bg-red-500 text-white my-2 rounded-lg text-sm p-2">
+                        {{$message}}
+                    </p>
+                    @enderror
                 </div>
                 <div class="mb-5">
                     <label for="password" class="mb-2 block uppercase text-gray-500 font-bold">
                         Password
                     </label>
                     <input type="password" id="password" name="password" placeholder="Tu Password de registro"
-                    class=" border p-3 w-full rounded-lg">
+                    class=" border p-3 w-full rounded-lg @error('password')
+                    border-red-500
+                @enderror"/>
+                    @error('password')
+                    <p class="bg-red-500 text-white my-2 rounded-lg text-sm p-2">
+                        {{$message}}
+                    </p>
+                    @enderror
                 </div>
                 <div class="mb-5">
                     <label for="password_confirmation" class="mb-2 block uppercase text-gray-500 font-bold">
                         Confirmar Password
                     </label>
                     <input type="password" id="password_confirmation" name="password_confirmation" placeholder="Confirma tu Password de registro"
-                    class=" border p-3 w-full rounded-lg">
+                    class=" border p-3 w-full rounded-lg @error('password_confirmation')
+                    border-red-500
+                @enderror">
+                    @error('password_confirmation')
+                    <p class="bg-red-500 text-white my-2 rounded-lg text-sm p-2">
+                        {{$message}}
+                    </p>
+                    @enderror
                 </div>
                 <input type="submit" value="Crear Cuenta"
                 class="bg-sky-600 text-center hover:bg-sky-700
