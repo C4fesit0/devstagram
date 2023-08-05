f48301a5dad9627a44dc71183a92452a38cfc142
Alan Lazo <al4nlaz0@gmail.com>
2023-08-04 23:39:43 -0300

    email and password validated added to login controller

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PostController;
 use App\Http\Controllers\Auth\RegisterController;
+use App\Http\Controllers\LoginController;
 
 /*
 |--------------------------------------------------------------------------
@@ -21,5 +22,8 @@ Route::get('/', function () {
 
 Route::get('/register',[RegisterController::class,'index'])->name('register');
 Route::post('/register',[RegisterController::class,'store']);
+
+Route::get('/login',[LoginController::class,'index'])->name('login');
+Route::post('/login',[LoginController::class,'store']);
 
 Route::get('/muro', [PostController::class, 'index'])->name('post.index');
