a799fa436ff31962871bd7c9add6451e315d18fd
Alan Lazo <al4nlaz0@gmail.com>
2023-08-11 18:54:17 -0300

    added intervention image package and upload folder for iimages

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PostController;
 use App\Http\Controllers\Auth\RegisterController;
+use App\Http\Controllers\ImagenController;
 use App\Http\Controllers\LoginController;
 use App\Http\Controllers\LogoutController;
 
@@ -30,3 +31,5 @@ Route::post('/logout', [LogoutController::class, 'store'])->name('logout');
 
 Route::get('/{user:username}', [PostController::class, 'index'])->name('post.index');
 Route::get('/posts/create', [PostController::class, 'create'])->name('post.create');
+
+Route::post('/imagenes', [ImagenController::class, 'store'])->name('imagenes.store');
